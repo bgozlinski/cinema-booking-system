79599592d26a2bf413c104cd8cde17b1bef11617
bgozlinski <bartlomiej.gozlinski@gmail.com>
2026-05-24 17:06:28 +0200

    feat(FR-16): token + refresh endpoints (US-30)

--- apps/accounts/api/urls.py
+++ apps/accounts/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from apps.accounts.api.views import RegisterView
+from apps.accounts.api.views import AuthTokenObtainPairView, RegisterView
 
 app_name = "accounts_api"
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("token/", AuthTokenObtainPairView.as_view(), name="token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
 ]
